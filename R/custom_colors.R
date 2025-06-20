23ac658cbe4bb4e7b6171339b678eac4039ee16a
jasav <119755709+jasav@users.noreply.github.com>
2025-06-20 08:52:44 +0200

    Initial commit

---
+++ R/custom_colors.R
@@ -0,0 +1,17 @@
+#' Function to return custom colors
+
+customblue <- "#348fd9"
+# use_data(customblue)
+custombluelight <- "#d1e0ed"
+# use_data(custombluelight)
+customred <- "#e6576d"
+# use_data(customred)
+customdarkred <- "#b5223f"
+# use_data(customdarkred)
+customgreen <- "#62d298"
+# use_data(customgreen)
+custompurple <- "#a869f5"
+# use_data(custompurple)
+customorange <- "#ff7936"
+# use_data(customorange)
+
---
+++ R/theme_glass.R
@@ -0,0 +1,18 @@
+#' A custom theme for ggplot2
+theme_glass <- function(){
+  ggplot2::theme_gray() +
+    ggplot2::theme(
+      strip.background = element_blank(),
+      strip.text = element_text(size = 11),
+      #panel.border = element_rect(fill = NA, linewidth = 1),
+      panel.border = element_blank(),
+      # axis.line = element_blank(),
+      axis.line = element_line(linewidth = 0.5, lineend = "square"),
+      axis.ticks = element_line(color = "black", lineend = "butt"),
+      axis.ticks.length = unit(0.2, "cm"),
+      panel.background = element_rect(color = NA, fill = "grey96"),
+      panel.grid = element_blank(),
+      axis.text = element_text(color = "black"),
+      plot.title = element_text(hjust = 0.5, size = 12)
+    )
+}
