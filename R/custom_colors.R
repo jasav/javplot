1e98c4b7b2d7d2198fc2932425704e4de5061f4f
jasav <119755709+jasav@users.noreply.github.com>
2025-06-20 09:39:45 +0200

    Changed custom colors to override default ggplot2

--- R/custom_colors.R
+++ R/custom_colors.R
@@ -1,17 +1,27 @@
-#' Function to return custom colors
+# From : https://stackoverflow.com/questions/10504724/change-the-default-colour-palette-in-ggplot
+custom.colors <- function(n) {
+  palette <- c("#348fd9", "#e6576d", "#a869f5", "#62d298",
+               "#ff7936", "#f0da16", "#b5223f", "#03fcb1",
+               "#35bfe6", "#f0da16", "#9ddb67", "#457996",
+               "darkslategray3", "lightgreen", "bisque",
+               "palevioletred1", "black", "gray79", "lightsalmon4",
+               "darkgoldenrod1")
+  if (n > length(palette))
+    warning('palette has duplicated colours')
+  rep(palette, length.out=n)
+}
 
-customblue <- "#348fd9"
-# use_data(customblue)
-custombluelight <- "#d1e0ed"
-# use_data(custombluelight)
-customred <- "#e6576d"
-# use_data(customred)
-customdarkred <- "#b5223f"
-# use_data(customdarkred)
-customgreen <- "#62d298"
-# use_data(customgreen)
-custompurple <- "#a869f5"
-# use_data(custompurple)
-customorange <- "#ff7936"
-# use_data(customorange)
+scale_custom <- function(aesthetics, ..., palette = custom.colors){ggplot2::discrete_scale(aesthetics = aesthetics, ..., palette = palette)}
+scale_colour_custom <- function(...) ggplot2::scale_custom("colour", ...)
+options(ggplot2.discrete.colour = scale_colour_custom)
 
+customcolors <- list(
+  blue = "#348fd9",
+  red = "#e6576d",
+  lightblue = "#d1e0ed",
+  darkred = "#b5223f",
+  green = "#62d298",
+  purple = "#a869f5",
+  orange = "#ff7936"
+)
+use_data(customcolors)
